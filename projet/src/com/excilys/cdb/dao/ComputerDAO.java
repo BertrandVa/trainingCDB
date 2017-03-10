bb132390518b4029f3f73bdf2028e3e8e82e3339
Bertrand Valentin <bvalentin@excilys.com>
2017-03-10 17:47:24 +0100

    ajout de la DAO computer

---
+++ projet/src/com/excilys/cdb/dao/ComputerDAO.java
@@ -0,0 +1,47 @@
+package com.excilys.cdb.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.excilys.cdb.java.Computer;
+
+public class ComputerDAO extends DAO<Computer> {
+
+	public ComputerDAO(Connection conn) {
+		super(conn);
+	}
+
+	@Override
+	public boolean create(Computer obj) {
+		return false;
+	}
+
+	@Override
+	public boolean delete(Computer obj) {
+		return false;
+	}
+
+	@Override
+	public boolean update(Computer obj) {
+		return false;
+	}
+
+	@Override
+	public Computer read(int id) {    
+	    Computer computer =new Computer(-1,null,null,null,null);
+	    try {
+	      ResultSet result = this.connect.createStatement().executeQuery("SELECT * FROM computer WHERE  = id" + id);
+	      if(result.first())
+	       computer = new Computer(
+	          id,
+	          result.getString("computer.name"),
+	          result.getString("computer.manufacturer"),
+	          result.getDate("computer.introduced"),
+	          result.getDate("computer.discontinued"));         
+	    } catch (SQLException e) {
+	      e.printStackTrace();
+	    }
+	    return computer;
+	  }
+}
