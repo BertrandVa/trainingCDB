9787cfbbfa061bb7c082b573ee40ac4aa0ec689b
Bertrand Valentin <bvalentin@excilys.com>
2017-03-13 12:06:59 +0100

    ajout test read Computer

--- projet/src/main/java/com/excilys/cdb/dao/ComputerDAO.java
+++ projet/src/main/java/com/excilys/cdb/dao/ComputerDAO.java
@@ -37,18 +37,32 @@ public class ComputerDAO {
 		return false;
 	}
 
-	public boolean update(Computer obj) {
-		return false;
+	public boolean update(Computer computer) {
+		boolean update = false;
+		try {
+			String SQL = "INSERT INTO computer (name, introduced, discontinued, company_id) VALUES (?, ?, ?, ?)";
+			java.sql.PreparedStatement statement = null;
+			statement = connection.prepareStatement(SQL);
+			statement.setString(1, computer.getName());
+			statement.setDate(2, computer.getIntroduceDate());
+			statement.setDate(3, computer.getDiscontinuedDate());
+			statement.setInt(4, computer.getManufacturer());
+			statement.executeUpdate();
+			update = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return update;
 	}
 
 	public Computer read(int id) {
 		Computer computer = new Computer(null, -1, null, null);
 		try {
 			ResultSet result = this.connection.createStatement().executeQuery(
-					"SELECT * FROM computer WHERE  = id" + id);
+					"SELECT * FROM computer WHERE id =" + id);
 			if (result.first())
 				computer = new Computer(result.getString("computer.name"),
-						result.getInt("computer.manufacturer"),
+						result.getInt("computer.company_id"),
 						result.getDate("computer.introduced"),
 						result.getDate("computer.discontinued"));
 			computer.setId(id);
--- projet/src/main/test/dbtest/ComputerDAOTest.java
+++ projet/src/main/test/dbtest/ComputerDAOTest.java
@@ -16,7 +16,7 @@ public class ComputerDAOTest {
 	private ConnectionFactory connection = ConnectionFactory.getInstance();
 	
 	@Test
-	public void testCreateComputer() {
+	public void testCreateComputer() {  //test de la création d'un ordinateur
 		boolean create = false;
 		String string = "test";
 		Computer computer = new Computer(string, 1, null, null);
@@ -31,19 +31,34 @@ public class ComputerDAOTest {
 		assertTrue(create);
 	}
 	
-
-	@Test
-	public void testDeleteComputer() {
-		fail("Not yet implemented");
-	}
-
 	@Test
 	public void testUpdateComputer() {
-		fail("Not yet implemented");
+		boolean create = false;
+		assertTrue(create);
 	}
 
 	@Test
-	public void testReadInt() {
+	public void testRead() {
+		Computer computer = new Computer(null,0,null,null);
+		Connection connect;
+		try {
+			connect = connection.getConnection();
+			ComputerDAO compDAO = new ComputerDAO(connect);
+			computer = compDAO.read(576);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+		assertEquals(computer.getName(),"test");
+		assertEquals(computer.getId(),576);
+		assertEquals(computer.getManufacturer(),1);
+		assertNull(computer.getDiscontinuedDate());
+		assertNull(computer.getIntroduceDate());
+		
+		
+	}
+	
+	@Test
+	public void testDeleteComputer() {
 		fail("Not yet implemented");
 	}
 
