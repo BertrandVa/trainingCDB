50f7e29bb794f69a847c60eb39a3503fd0c1cc94
Bertrand Valentin <bvalentin@excilys.com>
2017-03-13 17:44:04 +0100

    première user interface

--- projet/src/main/java/com/excilys/cdb/application/ConsoleApplication.java
+++ projet/src/main/java/com/excilys/cdb/application/ConsoleApplication.java
@@ -1,12 +1,251 @@
 package main.java.com.excilys.cdb.application;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import main.java.com.excilys.cdb.java.Computer;
+import main.java.com.excilys.cdb.services.ClientActions;
+
 public class ConsoleApplication {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean continu = true; // pour continuer
+		while (continu) {
+			ClientActions.menuPrincipal();
+			Scanner sc = new Scanner(System.in);
+			int choice = 0;
+			while (choice != 1 && choice != 2 && choice != 3 && choice != 4) {
+				System.out.println("Veuillez choisir une option");
+				boolean erreur;
+				do {
+					erreur = false;
+					try {
+						choice = sc.nextInt();
+						String buffer = sc.nextLine();
+					} catch (InputMismatchException e) {
+						erreur = true;
+						String buffer = sc.nextLine();
+					}
+				} while (erreur);
+			}
+			switch (choice) {
+			case 1:
+				ClientActions.listComputers();
+				boolean continu2 =true;
+				while(continu2){
+					ClientActions.menuOrdinateur();
+					Scanner myScan = new Scanner(System.in);
+					int choix = 0;
+					while (choix != 1 && choix != 2 && choix != 3 && choix != 4) {
+						System.out.println("Veuillez choisir une option");
+						boolean erreur;
+						do {
+							erreur = false;
+							try {
+								choix = myScan.nextInt();
+								String buffer = sc.nextLine();
+							} catch (InputMismatchException e) {
+								erreur = true;
+								String buffer = sc.nextLine();
+							}
+						} while (erreur);
+					}
+					switch (choix)
+					{
+					  case 1:
+						  int id;
+						  System.out.println("Veuillez entrer l'id du champ à modifier");	
+						  boolean erreur;
+						  do {
+							  erreur = false;
+								try {
+									id = sc.nextInt();
+									String buffer = sc.nextLine();
+								} catch (InputMismatchException e) {
+									erreur = true;
+									String buffer = sc.nextLine();
+								}
+							} while (erreur);						  
+						  System.out.println("Parfait ! entrez le nouveau nom de cet ordinateur");
+							boolean create;
+							Computer computer = new Computer(null, -1, null, null);
+							do {
+								erreur = false;
+								try {
+									computer.setName(sc.next());
+									String buffer = sc.nextLine();
+								} catch (InputMismatchException e) {
+									erreur = true;
+									String buffer = sc.nextLine();
+								}
+							} while (erreur);
+							System.out.println("Parfait ! entrez sa date d'acquisition yyyy MM dd ou 0 pour ignorer");
+							do {
+								erreur = false;
+								try {
+									 String dateString = sc.next();
+									 DateFormat formatter = new SimpleDateFormat("yyyy MM dd");
+									 Date date = formatter.parse(dateString);
+									computer.setDiscontinuedDate(date);
+									String buffer = sc.nextLine();
+								} catch (InputMismatchException e) {
+									erreur = true;
+									String buffer = sc.nextLine();
+								} catch (ParseException e) {
+									erreur = true;
+									String buffer = sc.nextLine();
+								}
+							} while (erreur);
+							System.out.println("Parfait ! entrez sa date de départ ou 0 pour ignorer");
+							do {
+								erreur = false;
+								try {
+									 String dateString = sc.next();
+									 DateFormat formatter = new SimpleDateFormat("yyyy MM dd");
+									 Date date = formatter.parse(dateString);
+									computer.setIntroduceDate(date);
+									String buffer = sc.nextLine();
+								} catch (InputMismatchException e) {
+									erreur = true;
+									String buffer = sc.nextLine();
+								} catch (ParseException e) {
+									erreur = true;
+									String buffer = sc.nextLine();
+								}
+							} while (erreur);
+							System.out.println("Parfait ! entrez l'id de son fournisseur");
+							do {
+								erreur = false;
+								try {
+									computer.setManufacturer(sc.nextInt());
+									String buffer = sc.nextLine();
+								} catch (InputMismatchException e) {
+									erreur = true;
+									String buffer = sc.nextLine();
+								}
+							} while (erreur);
+							create = ClientActions.updateComputer(computer);
+							if (create) {
+								System.out.println("L'ordinateur a été mis à jour");
+							} else {
+								System.out.println("Il y a eu un problème");
+							}						  
+					  case 2:
+						  System.out.println("Veuillez entrer l'id du champ à supprimer");	
+						  do {
+							  erreur = false;
+								try {
+									id = sc.nextInt();
+									String buffer = sc.nextLine();
+									ClientActions.deleteComputer(id);
+									System.out.println("Action effectuée");
+								} catch (InputMismatchException e) {
+									erreur = true;
+									String buffer = sc.nextLine();
+								}
+							} while (erreur);
+						  
+					  case 3:
+						  System.out.println("Veuillez entrer l'id du champ à afficher");	
+						  do {
+							  erreur = false;
+								try {
+									id = sc.nextInt();
+									String buffer = sc.nextLine();
+									ClientActions.showComputerDetails(id);
+								} catch (InputMismatchException e) {
+									erreur = true;
+									String buffer = sc.nextLine();
+								}
+							} while (erreur);
+						    
+					    break;        
+					  default:
+					    continu2=false;             
+					}					
+				}
+				break;
+			case 2:
+				ClientActions.listCompanies();
+				break;
+			case 3:
+				System.out.println("Parfait ! entrez le nom de ce nouvel ordinateur");
+				boolean erreur;
+				boolean create;
+				Computer computer = new Computer(null, -1, null, null);
+				do {
+					erreur = false;
+					try {
+						computer.setName(sc.next());
+						String buffer = sc.nextLine();
+					} catch (InputMismatchException e) {
+						erreur = true;
+						String buffer = sc.nextLine();
+					}
+				} while (erreur);
+				System.out.println("Parfait ! entrez sa date d'acquisition yyyy MM dd ou 0 pour ignorer");
+				do {
+					erreur = false;
+					try {
+						 String dateString = sc.next();
+						 DateFormat formatter = new SimpleDateFormat("yyyy MM dd");
+						 Date date = formatter.parse(dateString);
+						computer.setDiscontinuedDate(date);
+						String buffer = sc.nextLine();
+					} catch (InputMismatchException e) {
+						erreur = true;
+						String buffer = sc.nextLine();
+					} catch (ParseException e) {
+						erreur = true;
+						String buffer = sc.nextLine();
+					}
+				} while (erreur);
+				System.out.println("Parfait ! entrez sa date de départ ou 0 pour ignorer");
+				do {
+					erreur = false;
+					try {
+						 String dateString = sc.next();
+						 DateFormat formatter = new SimpleDateFormat("yyyy MM dd");
+						 Date date = formatter.parse(dateString);
+						computer.setIntroduceDate(date);
+						String buffer = sc.nextLine();
+					} catch (InputMismatchException e) {
+						erreur = true;
+						String buffer = sc.nextLine();
+					} catch (ParseException e) {
+						erreur = true;
+						String buffer = sc.nextLine();
+					}
+				} while (erreur);
+				System.out.println("Parfait ! entrez l'id de son fournisseur");
+				do {
+					erreur = false;
+					try {
+						computer.setManufacturer(sc.nextInt());
+						String buffer = sc.nextLine();
+					} catch (InputMismatchException e) {
+						erreur = true;
+						String buffer = sc.nextLine();
+					}
+				} while (erreur);
+				create = ClientActions.createComputer(computer);
+				if (create) {
+					System.out.println("L'ordinateur a été ajouté");
+				} else {
+					System.out.println("Il y a eu un problème");
+				}
+				break;
+			default:
+				continu = false;
+			}
+		}
 
 	}
 
--- projet/src/main/java/com/excilys/cdb/services/ClientActions.java
+++ projet/src/main/java/com/excilys/cdb/services/ClientActions.java
@@ -2,22 +2,71 @@ package main.java.com.excilys.cdb.services;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.com.excilys.cdb.dao.CompanyDAO;
 import main.java.com.excilys.cdb.dao.ComputerDAO;
+
 import main.java.com.excilys.cdb.dao.ConnectionFactory;
+import main.java.com.excilys.cdb.java.Company;
 import main.java.com.excilys.cdb.java.Computer;
 
 public class ClientActions {
 
-	private ConnectionFactory connection = ConnectionFactory.getInstance();
-	private Connection connect;
+	private static ConnectionFactory connection = ConnectionFactory
+			.getInstance();
+	private static Connection connect;
 
-	public void ListComputers() {
+	public static void menuPrincipal() {
+		System.out.println("Bonjour, bienvenue dans ce système de gestion des ordinateurs");
+		System.out.println("Pour lister les ordinateurs, tapez 1");
+		System.out.println("Pour lister les fournisseurs, tapez 2");
+		System.out.println("Pour créer un nouvel ordinateur, tapez 3");
+		System.out.println("Pour quitter, tapez 4");
+	}
+
+	public static void menuOrdinateur() {
+		System.out.println("Bonjour, bienvenue dans ce système de gestion des ordinateurs");
+		System.out.println("Pour mettre à jour une entrée, tapez 1");
+		System.out.println("Pour supprimer une entrée, tapez 2");
+		System.out.println("Pour obtenir plus de détails sur une entrée, tapez 3");
+		System.out.println("Pour revenir au menu principal, tapez 4");
+	}
+	
+	public static void listComputers() {
+		List<Computer> liste = new ArrayList<Computer>();
+		try {
+			connect = connection.getConnection();
+			ComputerDAO compDAO = new ComputerDAO(connect);
+			liste = compDAO.readAll();
+			for (int i = 0; i < liste.size(); ++i) {
+				System.out.println("id: "+liste.get(i).getId() +" nom: "+liste.get(i).getName());
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	};
 
-	public void ListCompanies() {
+	public static void listCompanies() {
+		List<Company> liste = new ArrayList<Company>();
+		try {
+			connect = connection.getConnection();
+			CompanyDAO compDAO = new CompanyDAO(connect);
+			liste = compDAO.readAll();
+			for (int i = 0; i < liste.size(); ++i) {
+				System.out.println("id: "+liste.get(i).getId() +" nom: "+liste.get(i).getName());
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	};
 
-	public void ShowComputerDetails(int id) { // implémenter les cas d'erreurs
+	public static void showComputerDetails(int id) { // implémenter les cas d'erreurs
 		Computer computer = new Computer(null, -1, null, null);
 		try {
 			connect = connection.getConnection();
@@ -38,13 +87,13 @@ public class ClientActions {
 				+ computer.getDiscontinuedDate());
 	};
 
-	public boolean CreateComputer(Computer computer) {// OK
-	// ne peut être créé sans nom
+	public static boolean createComputer(Computer computer) {// OK
+		// ne peut être créé sans nom
 		boolean fait = false;
 		try {
 			connect = connection.getConnection();
 			ComputerDAO compDAO = new ComputerDAO(connect);
-			if (computer.getName() != null) {
+			if (computer.getName() != null && computer.getName()!="") {
 				compDAO.create(computer);
 				fait = true;
 			}
@@ -54,7 +103,7 @@ public class ClientActions {
 		return fait;
 	};
 
-	public boolean UpdateComputer(Computer computer) { //OK
+	public static boolean updateComputer(Computer computer) { // OK
 		boolean fait = false;
 		try {
 			connect = connection.getConnection();
@@ -69,7 +118,7 @@ public class ClientActions {
 		return fait;
 	};
 
-	public boolean DeleteComputer(int id) {
+	public static boolean deleteComputer(int id) { // OK
 		boolean fait = false;
 		try {
 			connect = connection.getConnection();
